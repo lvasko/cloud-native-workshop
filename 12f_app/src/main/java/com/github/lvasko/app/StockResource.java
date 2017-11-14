25ea0367948597bfe292bc1d7e103617392cfa02
Lauri Vasko <jauv5oe7ST##>
2017-11-14 11:16:10 +0200

    Adding logging

--- 12f_app/src/main/java/com/github/lvasko/app/StockResource.java
+++ 12f_app/src/main/java/com/github/lvasko/app/StockResource.java
@@ -2,6 +2,8 @@ package com.github.lvasko.app;
 
 import com.github.lvasko.app.domain.model.StockItem;
 import com.github.lvasko.app.domain.model.StockItemRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
@@ -27,16 +29,21 @@ public class StockResource {
     }
 
 
+    private static final Logger logger = LoggerFactory.getLogger(StockResource.class);
+
     @RequestMapping(method = RequestMethod.GET, value = "/{id}")
     public StockItem stockItem(@PathVariable("id") Long id, HttpServletResponse response) {
+        logger.info("Starting search of stock item(id={}) search", id);
 
         StockItem stockItem = stockItemRepository.findOne(id);
 
         if (stockItem == null) {
+            logger.info("Stock item(id={}) has not been found", id);
             response.setStatus(HttpStatus.NOT_FOUND.value());
             return null;
         }
 
+        logger.info("Finishing search of stock item(id={}) search", id);
         return stockItem;
     }
 
